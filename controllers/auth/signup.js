1d32c35720b13728f5790e2902c9763ea6b2bb7d
Andrey-Baranyuk <andreybaranyuk86@gmail.com>
2022-07-26 11:18:19 +0300

    create auth

---
+++ controllers/auth/signup.js
@@ -0,0 +1,29 @@
+const { User } = require('../../models');
+const bcrypt = require('bcryptjs');
+
+const signup = async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+        res.status(409).json({
+            status: 'conflict',
+            code: 409,
+            message: 'Email in use'
+        });
+    }
+    const hashPassword = bcrypt.compare(password, 10);
+
+    await User.create({ email, password: hashPassword })
+    res.status(201).json({
+        status: 'success',
+        code: 201,
+        data: {
+            user: {
+                email,
+                'subscription': 'starter'
+            }
+        }
+    })
+}
+
+module.exports = signup;
---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const { ctrlWrapper } = require('../../middlewares/ctrlWrapper');
+
+router.post('/signup',)
+router.post('login', )
+router.get('/logout', )
+
+module.exports = router;
